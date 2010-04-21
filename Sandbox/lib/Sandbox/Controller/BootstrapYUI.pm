545b5b02b64f878b0bbda22db403d9e1c6e2d78a
Soren Dossing <netcom@sauber.net>
2010-04-21 21:20:43 +0900

    Example of YIU Bootstrap

---
+++ Sandbox/lib/Sandbox/Controller/BootstrapYUI.pm
@@ -0,0 +1,46 @@
+package Sandbox::Controller::BootstrapYUI;
+use Moose;
+use namespace::autoclean;
+
+BEGIN {extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+Sandbox::Controller::BootstrapYUI - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    #$c->response->body('Matched Sandbox::Controller::BootstrapYUI in BootstrapYUI.');
+  $c->detach( $c->view("TT_YUI") );
+
+}
+
+
+=head1 AUTHOR
+
+Soren Dossing
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
